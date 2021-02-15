ce10534fa6e50ee2593193d120482acb689e567a
stephanie <anie.12106@gmail.com>
2021-02-15 18:39:07 -0500

    Solución 10. Quitar espacios de una frase ingresada.

---
+++ src/com/company/Punto10.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import com.company.Input.Teclado;
+
+public class Punto10 {
+
+    public static void eliminarEspaciosBlanco(){
+        String frase;
+        System.out.println("Ingrese una frase linda: ");
+        frase= Teclado.input();
+        frase=frase.replaceAll(" ","");
+        System.out.println(frase);
+    }
+
+    public static void main(String[] args) {
+        Punto10.eliminarEspaciosBlanco();
+    }
+}
