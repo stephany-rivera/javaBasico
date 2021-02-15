0d5b6b9f459194235545c8b1e98f1bf6f8ac5b4a
stephanie <anie.12106@gmail.com>
2021-02-15 17:55:52 -0500

    Solución 6. Pares e impares con For.

---
+++ src/com/company/Punto6/Punto6.java
@@ -0,0 +1,23 @@
+package com.company.Punto6;
+
+public class Punto6 {
+
+    public void imparesPares() {
+        for (int i = 1; i < 101; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " es par");
+            } else {
+                System.out.println(i + "es impar");
+            }
+            i++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Punto6 p6= new Punto6();
+        p6.imparesPares();
+    }
+
+
+}
