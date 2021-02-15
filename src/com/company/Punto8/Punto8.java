598a731b5918410cb25fb3407b9fc85e1b3cc360
stephanie <anie.12106@gmail.com>
2021-02-15 18:13:54 -0500

    Solución 8. Días a la semana hábiles.

---
+++ src/com/company/Punto8/Punto8.java
@@ -0,0 +1,42 @@
+package com.company.Punto8;
+
+import com.company.Input.Teclado;
+
+public class Punto8 {
+
+    public void diaSemana() {
+        System.out.println("Digite el día de la semana en números: ");
+        Integer dia= Teclado.inputInteger();
+        switch (dia) {
+            case 1:
+                System.out.println("Día laboral");
+                break;
+            case 2:
+                System.out.println("Día laboral");
+                break;
+            case 3:
+                System.out.println("Día laboral");
+                break;
+            case 4:
+                System.out.println("Día laboral");
+                break;
+            case 5:
+                System.out.println("Día laboral");
+                break;
+            case 6:
+                System.out.println("Día NO laboral");
+                break;
+            case 7:
+                System.out.println("Día NO laboral");
+                break;
+            default:
+                System.out.println("No es un día valido");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Punto8 p8 = new Punto8();
+        p8.diaSemana();
+    }
+}
