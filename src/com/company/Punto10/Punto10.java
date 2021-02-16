f3e8623e20096d4930cc4a304da497c7060d59f7
stephanie <anie.12106@gmail.com>
2021-02-15 19:11:18 -0500

    Solución 11. Contar vocales de una frase.

--- src/com/company/Punto10/Punto10.java
+++ src/com/company/Punto10/Punto10.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.Punto10;
 
 import com.company.Input.Teclado;
 
---
+++ src/com/company/Punto11/Punto11.java
@@ -0,0 +1,38 @@
+package com.company.Punto11;
+
+import com.company.Input.Teclado;
+
+public class Punto11 {
+
+    public void contarVocales() {
+        System.out.println("Digita una frase");
+        String frase = Teclado.input();
+        Integer longitud = frase.length();
+        Integer contarA = 0, contarE = 0, contarI = 0, contarO = 0, contarU = 0, contarTotal = 0;
+
+        for (int i = 0; i < longitud; i++) {
+            if (frase.charAt(i) == 'a') {
+                contarA += 1;
+            } else if (frase.charAt(i) == 'e') {
+                contarE += 1;
+            } else if (frase.charAt(i) == 'i') {
+                contarI += 1;
+            } else if (frase.charAt(i) == 'o') {
+                contarO += 1;
+            } else if (frase.charAt(i) == 'u') {
+                contarU += 1;
+            }
+
+        }
+        contarTotal = contarA + contarE + contarI + contarO + contarU;
+        System.out.println("Cantidad vocales: " + contarTotal);
+        System.out.println("a: " + contarA + "\ne: " + contarE + "\ni:" + contarI + "\no: " + contarO + "\nu: " + contarU);
+
+    }
+
+    public static void main(String[] args) {
+        Punto11 p11= new Punto11();
+        p11.contarVocales();
+    }
+
+}
